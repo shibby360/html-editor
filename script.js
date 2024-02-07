83e060a5d3786307bc4f3ff46ba49143af3b75a8
shibby360 <shivank.chhaya@gmail.com>
2024-02-07 18:59:09 +0000

    added html running

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $('#run').click(function() {
-    var b64 = btoa($('#index.html').val())
-    $('#result').attr('src', 'data:text/html;base64,' + b64)
+  // var b64 = btoa()
+  // alert(b64)
+  $('#result').attr('srcdoc', $('#indexhtml').val())
 })
