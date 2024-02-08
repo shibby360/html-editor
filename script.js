6f8826449f56a2a84c2e7755efffdef5530d7089
shibby360 <shivank.chhaya@gmail.com>
2024-02-08 04:24:40 +0000

    added google

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ var editor = ace.edit('indexhtml', {
   wrap:true,
   theme:'ace/theme/monokai'
 })
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
