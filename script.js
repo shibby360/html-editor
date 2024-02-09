d983b70eb255ffa670d117d95022839ebb718f2b
shibby360 <shivank.chhaya@gmail.com>
2024-02-09 03:16:47 +0000

    added open file with file handle

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var saveFileHandle
 $('#run').click(function(ev) {
   console.log($('#indexhtml').val())
   $('#result').attr('srcdoc', editor.getValue())
@@ -7,13 +8,23 @@ var editor = ace.edit('indexhtml', {
   wrap:true,
   theme:'ace/theme/monokai'
 })
-$('#openfile').change(function(ev) {
-  function receivedText() {
-    editor.setValue(fr.result)
-  }
-  var fr = new FileReader()
-  fr.onload = receivedText
-  fr.readAsText(ev.target.files[0])
+$('#openfilepicker').click(function(ev) {
+  var filePick = showOpenFilePicker()
+  filePick.then(function(fileHandleLst) {
+    var fileHandle = fileHandleLst[0]
+    var fileprom = fileHandle.getFile()
+    fileprom.then(function(file) {
+      var fr = new FileReader();
+      fr.readAsText(file);
+      fr.onloadend = function() {
+        editor.setValue(fr.result)
+      }
+      saveFileHandle = fileHandle
+    })
+  })
+})
+$('#savebtn').click(function(ev) {
+  fileHandle.requestPermission({ mode: "readwrite" });
 })
 var auth2 = gapi.auth2.getAuthInstance();
 function onSignIn(googleUser) {
