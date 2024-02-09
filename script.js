bc2b6ad5716372d26b33dbb3ee0a1c7723260a3d
shibby360 <shivank.chhaya@gmail.com>
2024-02-09 04:40:27 +0000

    commented google sign in

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ $('#savebtn').click(async function(ev) {
   await writable.close()
   console.log('finished saving')
 })
-var auth2 = gapi.auth2.getAuthInstance();
+// var auth2 = gapi.auth2.getAuthInstance();
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
